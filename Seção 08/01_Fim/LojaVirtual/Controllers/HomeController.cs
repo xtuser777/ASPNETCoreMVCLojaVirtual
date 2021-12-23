31a19d657086b58bf3bf5e17d714a7da7b650224
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2021-12-23 20:42:20 -0300

    Criando página de login, cadastro e carrinho de compra.

--- Seção 08/01_Fim/LojaVirtual/Controllers/HomeController.cs
+++ Seção 08/01_Fim/LojaVirtual/Controllers/HomeController.cs
@@ -17,5 +17,20 @@ namespace LojaVirtual.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
     }
 }
