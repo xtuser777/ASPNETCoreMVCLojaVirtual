554fb2d7230ea1709d1f9c91f00ecc2464ab7e3d
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2022-01-23 20:57:12 -0300

    60. [ESTUDO] Configuração Banco de dados - Parte 1

---
+++ Seção 09/1_Fim/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            return View(GetProduto());
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Joque em 4k",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
