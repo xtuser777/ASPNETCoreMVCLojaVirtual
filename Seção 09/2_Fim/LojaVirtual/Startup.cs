59e8b6e58f90e0feca97b42239907a9645e36835
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2022-01-23 21:43:30 -0300

    62. [ESTUDO] Configuração Banco de dados - Parte 3

--- Seção 09/2_Fim/LojaVirtual/Startup.cs
+++ Seção 09/2_Fim/LojaVirtual/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using LojaVirtual.Database;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -33,6 +35,7 @@ namespace LojaVirtual
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
