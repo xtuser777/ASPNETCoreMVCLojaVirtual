a24f407af63854ec94de4cadf486752cbd5a68fb
Lucas S. Oliveira <lucaoxt@gmail.com>
2022-02-11 20:56:36 -0300

    68. [ESTUDO] Newsletter - FromForm e HttpGet e HttpPost

---
+++ Seção 09/8_Fim/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var news = new NewsletterEmail() { Email = "ribeiro@gmail.com" };
+            return View(news);
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm]NewsletterEmail newsletter)
+        {
+            //TODO - Adição no banco de dados
+
+            //TODO - Validações
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            Contato con = new Contato();
+
+            con.Nome = HttpContext.Request.Form["nome"];
+            con.Email = HttpContext.Request.Form["email"];
+            con.Texto = HttpContext.Request.Form["texto"];
+
+            var listaMensagens = new List<ValidationResult>();
+            var contexto = new ValidationContext(con);
+            var isvalid = Validator.TryValidateObject(con, contexto, listaMensagens, true);
+
+            if (isvalid)
+            {
+                try
+                {
+                    ContatoEmail.EnviarContatoPorEmail(con);
+
+                    ViewData["MSG_S"] = "Mensagem enviada com sucesso!";
+                }
+                catch (Exception e)
+                {
+                    ViewData["MSG_E"] = "Ocorreu um erro! Tente mais tarde.";
+
+                    //TODO - Implementar mensagem de LOG.
+                }
+            } 
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var texto in listaMensagens)
+                {
+                    sb.Append(texto.ErrorMessage + "<br />");
+                }
+
+                ViewData["MSG_E"] = sb.ToString();
+
+                ViewData["CONTATO"] = con;
+            }
+
+            return View("Contato");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ Seção 09/8_Fim/LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,44 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato con)
+        {
+            /*
+             * SMTP - serviço de envio
+             */
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new NetworkCredential("lucaoxt@gmail.com", "LonelyCrow777@CG");
+            smtpClient.EnableSsl = true;
+
+            string bodyMsg = string.Format(@"
+                <h2>Contato - LojaVirtual</h2> <br />
+                <b>Nome:</b> {0} <br />
+                <b>E-mail:</b> {1} <br />
+                <b>Texto:</b> {2} <br />
+                <br />
+                E-mail enviado automaticamente do site LojaVirtual.",
+                con.Nome, con.Email, con.Texto);
+
+            /*
+             * MailMessage - Constrói a mensagem
+             */
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("lucaoxt@gmail.com");
+            message.To.Add("lucaoxt@gmail.com");
+            message.Subject = "Contato - LojaVirtual - Email: " + con.Email;
+            message.IsBodyHtml = true;
+            message.Body = bodyMsg;
+
+            smtpClient.Send(message);
+        }
+    }
+}
