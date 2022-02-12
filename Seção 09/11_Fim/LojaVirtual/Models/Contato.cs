e6e7c3bcc14d82570de268387862d1cb162069d7
Lucas S. Oliveira <lucaoxt@gmail.com>
2022-02-12 15:31:39 -0300

    71. [ESTUDO] Newsletter - Usando Fragment para posicionar na tela

---
+++ Seção 09/11_Fim/LojaVirtual/Models/Contato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using LojaVirtual.Libraries.Lang;
+
+namespace LojaVirtual.Models
+{
+    public class Contato
+    {
+        [Required(ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E002")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E004")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(5, ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E002")]
+        [MaxLength(1000, ErrorMessageResourceType = typeof(Mensagens), ErrorMessageResourceName = "MSG_E003")]
+        public string Texto { get; set; }
+    }
+}
